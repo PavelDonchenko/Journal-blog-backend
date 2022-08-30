307185a4d1d93f0741a8d5931b4f1beb0159a61f
PavelDonchenko <przmld033@gmail.com>
2022-08-30 14:02:36 +0300

    add comments

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @Length(3)
   fullName: string;
+
+  @IsEmail(undefined, { message: 'Wrong email' })
   email: string;
+
+  @Length(6)
   password?: string;
 }
